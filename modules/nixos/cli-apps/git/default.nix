c018173750f1be1a99c765e81f4b2efd658780b6
Zhaohui Wang <18811365389@163.com>
2024-05-19 18:44:33 +0800

    Module: git remove gpg

--- modules/nixos/cli-apps/git/default.nix
+++ modules/nixos/cli-apps/git/default.nix
@@ -4,7 +4,6 @@ with lib;
 with lib.internal;
 let
   cfg = config.plusultra.tools.git;
-  gpg = config.plusultra.security.gpg;
   user = config.plusultra.user;
 in
 {
@@ -12,8 +11,6 @@ in
     enable = mkBoolOpt false "Whether or not to install and configure git.";
     userName = mkOpt types.str user.fullName "The name to configure git with.";
     userEmail = mkOpt types.str user.email "The email to configure git with.";
-    signingKey =
-      mkOpt types.str "9762169A1B35EA68" "The key ID to sign commits with.";
   };
 
   config = mkIf cfg.enable {
@@ -24,17 +21,13 @@ in
         enable = true;
         inherit (cfg) userName userEmail;
         lfs = enabled;
-        signing = {
-          key = cfg.signingKey;
-          signByDefault = mkIf gpg.enable true;
-        };
         extraConfig = {
           init = { defaultBranch = "main"; };
           pull = { rebase = true; };
           push = { autoSetupRemote = true; };
           core = { whitespace = "trailing-space,space-before-tab"; };
           safe = {
-            directory = "${config.users.users.${user.name}.home}/work/config";
+            # directory = "${config.users.users.${user.name}.home}/work/config";
           };
         };
       };
