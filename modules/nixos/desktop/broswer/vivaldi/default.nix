877b6d39688041ff5f3cc28223ef42226e4c76fe
Zhaohui Wang <18811365389@163.com>
2024-05-19 21:49:57 +0800

    vivaldi H.264

--- modules/nixos/desktop/broswer/vivaldi/default.nix
+++ modules/nixos/desktop/broswer/vivaldi/default.nix
@@ -7,12 +7,20 @@ in
 {
   options.plusultra.desktop.browser.vivaldi = with types; {
     enable = mkBoolOpt false "Whether to enable vivaldi browser.";
-    pkg = mkOpt package pkgs.vivaldi "The terminal to install.";
     default = mkBoolOpt false "Whether to set vivaldi as default browser.";
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = [ cfg.pkg ];
+    environment.systemPackages = with pkgs; [
+      (vivaldi.override {
+        proprietaryCodecs = true;
+        vivaldi-ffmpeg-codecs = vivaldi-ffmpeg-codecs;
+
+        # enabling this segfaults vivaldi at startup
+        # enableWidevine = true;
+        # widevine-cdm = vivaldi-pkgs.widevine-cdm;
+      })
+    ];
 
     environment.variables = mkIf cfg.default {
       BROWSER = "vivaldi";
