d0d1101165fa02598f7396953e1172eeb46d6717
Zhaohui Wang <18811365389@163.com>
2024-05-20 09:33:12 +0800

    Module: nix/cache

---
+++ modules/nixos/nix/cache/default.nix
@@ -0,0 +1,18 @@
+{ config, lib, ... }:
+
+with lib;
+with lib.plusultra;
+let
+  cfg = config.plusultra.nix-cache;
+in
+{
+  options.plusultra.nix-cache = {
+    enable = mkEnableOption "Plus Ultra public cache";
+  };
+
+  config = mkIf cfg.enable {
+    plusultra.nix.extra-substituters = {
+      "https://tuna.tsinghua.edu.cn/nix-channels/store".key = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
+    };
+  };
+}
