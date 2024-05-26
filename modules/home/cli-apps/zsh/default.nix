ad5650e403d7be702808db26544da60ad0ad91ec
Zhaohui Wang <18811365389@163.com>
2024-05-27 01:03:24 +0800

    home module zsh

---
+++ modules/home/cli-apps/zsh/default.nix
@@ -0,0 +1,21 @@
+inputs @ {
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.plusultra; let
+  cfg = config.plusultra.cli-apps.zsh;
+in {
+  options.plusultra.cli-apps.zsh = with types; {
+    enable = mkBoolOpt false "Whether or not to enable zsh config.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.zsh = {
+      enable = true;
+    };
+  };
+}
