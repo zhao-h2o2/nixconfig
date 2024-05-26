f6dad0de0d95f57760f1a6d97cb91d0269bb65ea
Zhaohui Wang <18811365389@163.com>
2024-05-26 22:03:52 +0800

    enable neovim home module

---
+++ modules/home/cli-apps/neovim/default.nix
@@ -0,0 +1,27 @@
+inputs @ {
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.plusultra; let
+  cfg = config.plusultra.cli-apps.neovim;
+in {
+  options.plusultra.cli-apps.neovim = with types; {
+    enable = mkBoolOpt false "Whether or not to enable neovim.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      withPython3 = true;
+      withNodeJs = true;
+      withRuby = true;
+    };
+  };
+}
